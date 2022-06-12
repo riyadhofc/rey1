525f6e38180624576bb0394762ce4fe6364c5f94
riyadhofc <107257372+riyadhofc@users.noreply.github.com>
2022-06-12 07:50:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ global.APIs = {
 
 // Isi Api Mu Di sini
 global.APIKeys = {
-	'https://zenzapis.xyz': 'BagasPrdn',
+	'https://zenzapis.xyz': 'hdiiofficial',
 }
 //Thank Fax Bot
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
